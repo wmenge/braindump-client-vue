39503df995695eb212613851e238052748958a28
wilco menge <wilcomenge@gmail.com>
2020-10-04 16:39:34 +0200

    refactoring: move app into separate component

---
+++ public/app/app.js
@@ -0,0 +1,54 @@
+import resource from '../helpers/resource.js';
+import notebookList from '../components/notebook-list.js';
+import notebookListItem from '../components/notebook-list-item.js';
+import noteBookHeader from '../components/notebook-header.js';
+import noteListItem from '../components/note-list-item.js';
+
+import { notificationList } from '../components/notification-list.js';
+
+import navigationBar from '../components/navigation-bar.js';
+import createNotebookModal from '../components/create-notebook-modal.js';
+import settingsModal from '../components/settings-modal.js';
+import { confirmationModalComponent } from '../components/confirmation-modal.js';
+import router from './router.js';
+
+Vue.component('notebook-list', notebookList);
+Vue.component('notebook-list-item', notebookListItem);
+Vue.component('note-list-item', noteListItem);
+Vue.component('navigation-bar', navigationBar);
+Vue.component('notebook-header', noteBookHeader);
+Vue.component('notification-list', notificationList);
+Vue.component('create-notebook-modal', createNotebookModal);
+Vue.component('settings-modal', settingsModal);
+Vue.component('confirmation-modal', confirmationModalComponent);
+
+// App
+let app = new Vue({
+    data: {
+        // still needed? 
+        notebook: { title: '' },
+        note: { title: null, content: null },
+        refNote: { title: null, content: null },
+    },
+    computed: {
+        dirty() {
+            //console.log(this.note.title, this.refNote.title);
+            //console.log(this.note.content, this.refNote.content);
+            return !(this.note.title == this.refNote.title && this.note.content == this.refNote.content);
+        }
+    },
+    methods: {
+        setNote(data) {
+            this.note = data;
+            this.refNote = {...data};
+        },
+        clean() {
+            this.note = { title: null, content: null };
+            this.refNote = { title: null, content: null };
+        }
+    },
+    router: router,
+    el: '#app'
+});
+
+export default app;
